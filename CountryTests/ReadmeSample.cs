53655267c97d1e1cd1b6c42918757c9bff224b79
Jan Marques <jan@janmarques.net>
2017-07-17 23:31:39 +0200

    Add sample + fix github link

---
+++ CountryTests/ReadmeSample.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using ExtendedIsoCountries;
+
+namespace CountryTests
+{
+    public class ReadmeSample
+    {
+        [Fact]
+        public void ReadmeExample_Spain()
+        {
+            var country = Country.GetBy2CharacterCode("ES");
+            Assert.Equal(country.Alpha2Code, "ES");
+            Assert.Equal(country.Alpha3Code, "ESP");
+            Assert.Equal(country.NumericCode, 724);
+            Assert.Equal(country.Name, "Spain");
+            Assert.Equal(country.Adjective, "Spanish"); // (a Spanish beer)
+            Assert.Equal(country.Demonym, "Spaniard"); // (a Spaniard living in Barcelona)
+            Assert.Equal(country.HasAdjective, true);
+            Assert.Equal(country.HasDemonym, true);
+        }
+    }
+}
